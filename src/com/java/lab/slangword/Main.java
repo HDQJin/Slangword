0a88ebd6218e389969f5b29d9adc5cf59c5515f0
HDQJin <91780366+HDQJin@users.noreply.github.com>
2021-12-12 17:19:02 +0700

    second update

--- src/com/java/lab/slangword/Main.java
+++ src/com/java/lab/slangword/Main.java
@@ -1,6 +1,7 @@
 package com.java.lab.slangword;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * com.java.slangword
@@ -11,8 +12,57 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Slangword s = new Slangword();
+        Scanner scanner = new Scanner(System.in);
+        int chosen, exit = 0;
+        while(true) {
+            s.menu();
+            do {
+                System.out.print("Enter the chosen: ");
+                chosen = scanner.nextInt();
+            }while(chosen < 0 || chosen > 11);
 
-        s.searchSlang();
+            switch (chosen)
+            {
+                case 0:
+                    System.out.println("You exited!");
+                    exit = 1;
+                    break;
+                case 1:
+                    s.searchSlang();
+                    break;
+                case 2:
+                    s.searchDestination();
+                    break;
+                case 3:
+                    s.history();
+                    break;
+                case 4:
+                    s.add();
+                    break;
+                case 5:
+                    s.edit();
+                    break;
+                case 6:
+                    s.delete();
+                    break;
+                case 7:
+                    s.reset();
+                    break;
+                case 8:
+                    s.random();
+                    break;
+//                case 9:
+//
+//                    break;
+//                case 10:
+//
+//                    break;
+
+            }
+            if(exit == 1)
+                break;
+        }
+
     }
 
 }
--- src/com/java/lab/slangword/Slangword.java
+++ src/com/java/lab/slangword/Slangword.java
@@ -1,9 +1,12 @@
 package com.java.lab.slangword;
 
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -20,7 +23,7 @@ public class Slangword {
      * @throws IOException
      */
     public Slangword() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Admin\\Documents\\19127530_Lab02\\src\\com\\java\\slangword\\slang.txt")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Admin\\Documents\\SlangWord\\src\\com\\java\\lab\\slangword\\slang.txt")));
         String line, str[];
 
         while ((line = br.readLine()) != null) {
@@ -29,7 +32,7 @@ public class Slangword {
             map.put(str[0], str[1]);
 
         }
-
+        br.close();
     }
 
 
@@ -41,11 +44,169 @@ public class Slangword {
 
     }
 
-    public void searchSlang()
+    public static void menu()
     {
+        System.out.println("-------------------MENU-----------------");
+        System.out.println("1: search by slang word");
+        System.out.println("2: search by the destination");
+        System.out.println("3: show history");
+        System.out.println("4: add to slang word");
+        System.out.println("5: edit slang word");
+        System.out.println("6: delete slang word");
+        System.out.println("7: reset slang word of root");
+        System.out.println("8: random slang word");
+        System.out.println("9: game: display slang word");
+        System.out.println("10: game: display destination");
+        System.out.println("0: exit");
+    }
+
+    public void searchSlang() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the key: ");
         String str = scanner.nextLine();
-        System.out.println(map.get(str));
+        if(map.get(str) != null)
+            System.out.println(map.get(str));
+        else
+            System.out.println("Not exist!");
+        BufferedWriter bout = new BufferedWriter(new FileWriter("C:\\Users\\Admin\\Documents\\SlangWord\\src\\com\\java\\lab\\slangword\\save.txt", true));
+        bout.write(str);
+        bout.write("\n");
+        bout.close();
+    }
+
+    public void searchDestination()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the destination: ");
+        String str = scanner.nextLine();
+        for (String i : map.keySet()) {
+            if(map.get(i).equalsIgnoreCase(str)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+    public void history() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Admin\\Documents\\SlangWord\\src\\com\\java\\lab\\slangword\\save.txt")));
+        String line;
+        while((line = br.readLine()) != null)
+            System.out.println(line);
+        br.close();
+    }
+
+    public void add()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the slang word you want to add: ");
+        String str = scanner.nextLine();
+        System.out.print("Enter the destination you want to add: ");
+        String des = scanner.nextLine();
+        if(map.containsKey(str) == true)
+        {
+            System.out.println("1: overwrite");
+            System.out.println("2: duplicate");
+            System.out.print("Enter the chosen: ");
+            int number = scanner.nextInt();
+            switch(number)
+            {
+                case 1:
+                    map.put(str, des);
+                    break;
+                case 2:
+                    map.put(str, map.get(str) + " | " + des);
+                    break;
+
+            }
+        }
+        else
+            map.put(str, des);
+    }
+
+    public void edit()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the slang word you want to edit: ");
+        String str = scanner.nextLine();
+        int chosen;
+        do {
+            System.out.println("1: edit slang word");
+            System.out.println("2: edit destination");
+            System.out.print("Enter the chosen: ");
+            chosen = scanner.nextInt();
+        }while(chosen < 1 || chosen > 2);
+
+        switch (chosen)
+        {
+            case 1:
+                System.out.print("Enter slang word: ");
+                String temp1 = scanner.nextLine();
+                String newSlw = scanner.nextLine();
+                String s = map.get(str);
+                map.remove(str);
+                map.put(newSlw, s);
+                break;
+            case 2:
+                System.out.print("Enter destination: ");
+                String temp2 = scanner.nextLine();
+                String newDes = scanner.nextLine();
+                map.remove(str);
+                map.put(str, newDes);
+                break;
+        }
+    }
+
+    public void delete()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the slang word: ");
+        String str = scanner.nextLine();
+        if(map.containsKey(str) == true)
+        {
+            String confirm;
+            do {
+                System.out.print("You want to delete! - Y/N: ");
+                confirm = scanner.nextLine();
+            }while(!confirm.equals("y") && !confirm.equals("Y") && !confirm.equals("n") && !confirm.equals("N"));
+            if(confirm.equals("y") || confirm.equals("Y")) {
+                map.remove(str);
+                System.out.println("Delete success!");
+            }
+        }
+        else
+            System.out.println("Slang word not exit!");
+    }
+
+    public void reset() throws IOException {
+        map.clear();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Admin\\Documents\\SlangWord\\src\\com\\java\\lab\\slangword\\slang.txt")));
+        String line, str[];
+
+        while ((line = br.readLine()) != null) {
+
+            str = line.split("`");
+            map.put(str[0], str[1]);
+
+        }
+        br.close();
+    }
+
+    public void random()
+    {
+        Random random = new Random();
+        int number = random.nextInt(map.size()), index = 0;
+        for (String i : map.keySet()) {
+            if(index == number) {
+                System.out.println(i + "  -  " + map.get(i));
+                break;
+            }
+            index++;
+        }
+    }
+
+    public void gameSlangWord()
+    {
+
     }
 }
+
